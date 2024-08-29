fe0aad7e64685aa86054193575d2aa5677d3cd93
Gavin Noronha <gavinln@hotmail.com>
2024-08-29 15:43:05 -0700

    clean up code using pre-commit

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -1,5 +1,12 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request,
-                   url_for)
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
@@ -48,54 +55,59 @@ def create():
 
 
 def get_post(id, check_author=True):
-    post = get_db().execute("""
+    post = (
+        get_db()
+        .execute(
+            """
         SELECT p.id, title, body, created, author_id, username
         FROM post p JOIN user u ON p.author_id = u.id
         WHERE p.id = ?""",
-        (id,)
-    ).fetchone()
+            (id,),
+        )
+        .fetchone()
+    )
 
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post["author_id"] != g.user["id"]:
         abort(403)
 
     return post
 
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route("/<int:id>/update", methods=("GET", "POST"))
 @login_required
 def update(id):
     post = get_post(id)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+    if request.method == "POST":
+        title = request.form["title"]
+        body = request.form["body"]
         error = None
 
         if not title:
-            error = 'Title is required.'
+            error = "Title is required."
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
+                "UPDATE post SET title = ?, body = ?" " WHERE id = ?",
+                (title, body, id),
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for("blog.index"))
 
-    return render_template('blog/update.html', post=post)
+    return render_template("blog/update.html", post=post)
 
-@bp.route('/<int:id>/delete', methods=('POST',))
+
+@bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute("DELETE FROM post WHERE id = ?", (id,))
     db.commit()
-    return redirect(url_for('blog.index'))
+    return redirect(url_for("blog.index"))
